f6815ec3a3857932df9cb1a129c5fb9c02000ebc
Kirill Soldatov <k.igorevitch88@yandex.ru>
2024-03-31 12:11:28 +0300

    Practice useParams + find method

--- src/components/pages/Model.tsx
+++ src/components/pages/Model.tsx
@@ -1,17 +1,25 @@
 import React from 'react';
 import {adidasArr} from "../../App";
 import {useParams} from "react-router-dom";
+import {Error404} from "./Error404";
 
 export const Model = () => {
     const params = useParams()
 
+    const currentModel = adidasArr.find(model => model.id === Number(params.id))
+
     return (
         <div>
-            <h3>{adidasArr[Number(params.id)].model}</h3>
-            <div>{adidasArr[Number(params.id)].collection}</div>
-            <div>{adidasArr[Number(params.id)].price}</div>
-            <img src={adidasArr[Number(params.id)].picture}
-                 alt={`${adidasArr[Number(params.id)].model} from ${adidasArr[Number(params.id)].collection}`}/>
+            {currentModel
+                ?
+                <>
+                    <h2>{currentModel.model}</h2>
+                    <div>{currentModel.collection}</div>
+                    <div>{currentModel.price}</div>
+                    <img src={currentModel.picture} alt={currentModel.collection}/>
+                </>
+                : <h2>Absent model</h2>
+            }
         </div>
     );
 };
